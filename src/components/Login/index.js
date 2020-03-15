e9c0d867d5848035c4aa20c969690db76b3a319d
Caio Felix <caiohalcsik@gmail.com>
2020-03-15 10:31:38 -0300

    WIP

---
+++ src/components/Login/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const Login = () => {
+  return(
+    <>
+      <Container>
+        <span> Text </span>
+        <input></input>
+        <span> Text </span>
+        <input></input>
+        <button> Teste </button>
+      </Container>
+    </>
+  )
+}
+
+export default Login;
